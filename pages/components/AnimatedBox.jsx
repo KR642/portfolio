52c1538f47d01e5598e29d46ca2190d9a378a6da
KR642 <KR642@live.mdx.ac.uk.com>
2024-01-31 15:57:42 +0000

    resume and card added

--- pages/components/AnimatedBox.jsx
+++ pages/components/AnimatedBox.jsx
@@ -19,7 +19,7 @@ return (
       className={`bg-text animate-comein min-[320px]:w-[15rem] md:w-[20rem] h-[20rem] rounded-2xl absolute -top-[30rem] flex justify-between place-content-between flex-col items-center`}
   >
     <div className='translate-y-20 rotate-90'>
-    <p className='text-sm font-bold  text-background'>{`AKSHAY'S PORTFOLIO`}</p>
+    <p className='text-sm font-bold  text-background'>{`KRISHNAPRIYA'S PORTFOLIO`}</p>
     <p className='text-sm font-medium '>Key accepted</p>
     </div>
     <RiRfidLine className='z-50 text-[5rem] text-ascent animate-ping'/>
